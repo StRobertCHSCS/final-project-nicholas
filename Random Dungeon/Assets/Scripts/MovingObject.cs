12badc4236523c41c0adaccfc49475fea8911719
NSmith019 <Niholas.Smith21@ycdsbk12.ca>
2020-01-11 23:28:17 -0500

    create and start moving objects script

---
+++ Random Dungeon/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour
+{
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1/moveTime;
+
+    }
+
+    protected IEnumerator SmoothMovement(Vector3 end) 
+    {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        while (sqrRemainingDistance > float.Epsilon)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            rb2D.MovePosition(newPosition);
+            sqrRemainingDistance = (transform.position-end).sqrMagnitude;
+            yield return null;
+        }
+    }
+
+    protected abstract void OnCantMove <T> (T component)
+        where T : Component;
+        
+
+}
