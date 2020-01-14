6840c9b3680223597e2859406b071e72de3d9da7
NSmith019 <Niholas.Smith21@ycdsbk12.ca>
2020-01-13 22:56:56 -0500

    finish moving objects script

--- Random Dungeon/Assets/Scripts/MovingObject.cs
+++ Random Dungeon/Assets/Scripts/MovingObject.cs
@@ -20,6 +20,23 @@ public abstract class MovingObject : MonoBehaviour
 
     }
 
+    protected bool Move(int xDirection, int yDirection, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDirection, yDirection);
+
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        boxCollider.enabled = true;
+
+        if (hit.transform == null)
+        {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+        return false;
+    }
+
     protected IEnumerator SmoothMovement(Vector3 end) 
     {
         float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
@@ -30,6 +47,25 @@ public abstract class MovingObject : MonoBehaviour
             rb2D.MovePosition(newPosition);
             sqrRemainingDistance = (transform.position-end).sqrMagnitude;
             yield return null;
+        }
+    }
+
+    protected virtual void AttemptMove <T>(int xDirection, int yDirection)
+        where T : Component
+    {
+        RaycastHit2D hit;
+        bool canMove = Move(xDirection, yDirection, out hit);
+
+        if (hit.transform == null)
+        {
+            return;
+        }
+
+        T hitComponent = hit.transform.GetComponent<T>();
+
+        if(!canMove && hitComponent != null)
+        {
+            OnCantMove(hitComponent);
         }
     }
 
