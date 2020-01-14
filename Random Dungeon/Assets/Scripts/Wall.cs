819fc7b8f7ce38becd03025b039ede8aca01e2e7
NSmith019 <Niholas.Smith21@ycdsbk12.ca>
2020-01-13 23:35:25 -0500

    begin wall script (unfinished)

---
+++ Random Dungeon/Assets/Scripts/Wall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite damageSprite;
+    public int hitPoints = 4;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void DamageWall(int loss)
+    {
+        if (hitPoints == 2)
+        {
+            spriteRenderer.sprite = damageSprite;
+        }
+        hitPoints -= loss;
+        if (hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
