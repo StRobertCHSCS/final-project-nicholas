09c555196e74348c50727bcb593a9027493eac6f
NSmith019 <Niholas.Smith21@ycdsbk12.ca>
2020-01-21 00:53:34 -0500

    gamemanager and ball scripts
    ball script not done yet

---
+++ 2.5D pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private float startWaitTime = 3f;
+    private float ballSpeed = 10f;
+    private float startCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startCount = startWaitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startCount > 0f)
+        {
+            startCount = startWaitTime - Time.time;
+        }
+        else
+            gameObject.transform.position = new Vector3(transform.position.x + (ballSpeed * Time.deltaTime), transform.position.y, transform.position.z);
+    }
+
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter(Collision other)
+    {
+        ballSpeed *= -1f;
+    }
+}
---
+++ 2.5D pong/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject paddle1;
+    public GameObject paddle2;
+    public GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(paddle1);
+        Instantiate(paddle2);
+        Instantiate(ball);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
