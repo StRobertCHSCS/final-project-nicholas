19055a1b04d919b3f744818330af3ce4ce6955ff
NSmith019 <Niholas.Smith21@ycdsbk12.ca>
2020-01-22 19:53:13 -0500

    paddle movement scripts

---
+++ 2.5D pong/Assets/Scripts/Paddle1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle1 : MonoBehaviour
+{
+    float direction;
+    float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction = 1;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction = -1;
+        }
+        else
+        {
+            direction = 0;
+        }
+        transform.position = new Vector3(transform.position.x, transform.position.y + direction * speed * Time.deltaTime, transform.position.z);
+    }
+}
---
+++ 2.5D pong/Assets/Scripts/Paddle2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle2 : MonoBehaviour
+{
+    float direction;
+    float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            direction = -1;
+        }
+        else
+        {
+            direction = 0;
+        }
+        transform.position = new Vector3(transform.position.x, transform.position.y + direction * speed * Time.deltaTime, transform.position.z);
+    }
+}
