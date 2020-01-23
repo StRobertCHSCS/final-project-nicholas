6b161e64659250efa1e709081369d76e16cf16be
NSmith019 <Niholas.Smith21@ycdsbk12.ca>
2020-01-22 22:35:58 -0500

    allow paddles to change the y direction of the ball

--- 2.5D pong/Assets/Scripts/Ball.cs
+++ 2.5D pong/Assets/Scripts/Ball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private float startWaitTime = 3f;
+    [SerializeField]
     private float ballSpeed = 5f;
     public float startCount;
     [SerializeField]
@@ -33,9 +34,9 @@ public class Ball : MonoBehaviour
         }
 
         if (startCount < 0)
+        {
             gameObject.transform.position = new Vector3(transform.position.x + xdir * ballSpeed * Time.deltaTime, transform.position.y + ydir * ballSpeed * Time.deltaTime, transform.position.z);
-
-        
+        }
     }
 
     /// <summary>
@@ -48,10 +49,38 @@ public class Ball : MonoBehaviour
         if (other.gameObject.tag == "Paddle")
         {
             xdir *= -1f;
+            if (transform.position.y > other.transform.position.y)
+            {
+                ydir = 1;
+            }
+            else if (transform.position.y < other.transform.position.y)
+            {
+                ydir = -1;
+            }
+            else
+            {
+                ydir = 0;
+            }
         }
         else if (other.gameObject.tag == "Walls")
         {
             ydir *= -1f;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.tag == "Player1EndZone")
+        {
+            // one point to player 1
+            // reset ball
+            // on keypress space, restart it
+        }
+        else if (other.gameObject.tag == "Player2EndZone")
+        {
+            // one point to player 2
+            // reset ball
+            // on keypress space, restart it
         }
     }
 }
