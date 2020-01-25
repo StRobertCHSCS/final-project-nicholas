bac324779897d0a348827f42a26a74a4f3698f36
NSmith019 <Niholas.Smith21@ycdsbk12.ca>
2020-01-25 04:22:56 -0500

    i forgot to save this file when i commited the comments

--- 2.5D pong/Assets/Scripts/Player2Score.cs
+++ 2.5D pong/Assets/Scripts/Player2Score.cs
@@ -5,18 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class Player2Score : MonoBehaviour
 {
+    // initial variable denfinitions
     TextMesh textMesh;
     int score = 0;
     void Start()
     {
+        /*
+            called before the first frame update
+        */
+        
+        // sets the textmesh component of the gameobject its attached to the textmesh variable
         textMesh = gameObject.GetComponent<TextMesh>();
     }
     public void UpdatePlayer2Score()
     {
+        /*
+            is used by the ball's sript to update player 2's score
+        */
+
+        // adds 1 to the player's score
         score ++;
+
+        // sets the text in the text mesh attached to the gameobject to the player's score as a string
         textMesh.text = score.ToString();
-        if (score >= 10)
+
+        // checks if the player's score is equal to 10
+        if (score == 10)
         {
+            // closes the current scene and loads the player 2 win screen scene
             SceneManager.LoadScene("Player2WinScreen", LoadSceneMode.Single);
         }
     }
